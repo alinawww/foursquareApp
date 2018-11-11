45dca75bfdc647bb1bce20ef247a9470fb69d13c
Alina Tudose <alina.tudose@travelbird.com>
2018-11-11 17:14:20 +0100

    Updates

---
+++ precache-manifest.11d5cc718a16dfd8506c93d3de9d5b49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b438d328058989ad593",
+    "url": "/foursquareApp/static/js/runtime~main.6b438d32.js"
+  },
+  {
+    "revision": "c05290e928866be180db",
+    "url": "/foursquareApp/static/js/main.c05290e9.chunk.js"
+  },
+  {
+    "revision": "af5eacd7e3fb440d3d57",
+    "url": "/foursquareApp/static/js/1.af5eacd7.chunk.js"
+  },
+  {
+    "revision": "c05290e928866be180db",
+    "url": "/foursquareApp/static/css/main.f8ce8ed0.chunk.css"
+  },
+  {
+    "revision": "69b7770cc54503d3fe1584c2c539f9ac",
+    "url": "/foursquareApp/index.html"
+  }
+];
